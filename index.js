2fb9d9bef3babb032968f00c72a7b35c13e7d3a0
Eu4ng <eu4ng97@gmail.com>
2024-01-25 19:56:50 +0900

    feat(Action): lint-pull-request 추가 (#1)

---
+++ index.js
@@ -0,0 +1,19 @@
+﻿const core = require('@actions/core');
+const github = require('@actions/github');
+const lintPullRequest = require('./src/lintPullRequest');
+
+async function run()
+{
+    // Pull Request 제목 추출
+    const pullRequest = github.context.payload.pull_request;
+    if (!pullRequest) { throw new Error("Pull Request 만 검사가 가능합니다"); }
+
+    // Conventional Commits Config
+    const config = process.env.INPUT_CONFIG;
+    core.info(`config ${config}`);
+
+    // Pull Request 제목이 Conventional Commits 규칙을 준수하는지 검사
+    await lintPullRequest(pullRequest.title);
+}
+
+run();
---
+++ src/lintPullRequest.js
@@ -0,0 +1,38 @@
+﻿const commitlintConfig = require('conventional-changelog-conventionalcommits');
+const conventionalCommitTypes = require('conventional-commit-types');
+const conventionalCommitsParserOptions = require('conventional-commits-parser').sync;
+
+const defaultTypes = Object.keys(conventionalCommitTypes.types);
+
+module.exports = async function lintPullRequest(commit, types = defaultTypes) {
+    const {parserOpts} = await commitlintConfig();
+    const result = conventionalCommitsParserOptions(commit, parserOpts);
+
+    function printAvailableTypes() {
+        return `Available types:\n${types
+            .map((type) => {
+                let bullet = ` - ${type}`;
+
+                if (types === defaultTypes) {
+                    bullet += `: ${conventionalCommitTypes.types[type].description}`;
+                }
+
+                return bullet;
+            })
+            .join('\n')}`;
+    }
+
+    if (!result.type) {
+        throw new Error(
+            `No release type found in pull request title "${commit}". Add a prefix to indicate what kind of release this pull request corresponds to (see https://www.conventionalcommits.org/).\n\n${printAvailableTypes()}`
+        );
+    }
+
+    if (!types.includes(result.type)) {
+        throw new Error(
+            `Unknown release type "${
+                result.type
+            }" found in pull request title "${commit}". \n\n${printAvailableTypes()}`
+        );
+    }
+};
